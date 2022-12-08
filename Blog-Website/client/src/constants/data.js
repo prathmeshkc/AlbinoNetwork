45cfad86679b158f522c15ce25ff50fb687dd6dd
Grishma <karkera.g@northeastern.edu>
2022-12-07 21:25:44 -0500

    New Changes

--- Blog-Website/client/src/constants/data.js
+++ Blog-Website/client/src/constants/data.js
@@ -1,6 +1,6 @@
 export const categories = [
     { id: 1, type: "Music" },
-    { id: 2, type: "Movies" },
+    { id: 2, type: "Depression" },
     { id: 3, type: "Sports" },
     { id: 4, type: "Tech" },
     { id: 5, type: "Fashion" }

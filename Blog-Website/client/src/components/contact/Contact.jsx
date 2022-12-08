f72aa1931a95e2131e18167115c272ae1cb1190b
Ritesh <choudhary.rit@northeastern.edu>
2022-12-07 21:32:33 -0500

    Updated contents of Contact.jsx

--- Blog-Website/client/src/components/contact/Contact.jsx
+++ Blog-Website/client/src/components/contact/Contact.jsx
@@ -1,5 +1,5 @@
 
-import { Box, styled, Typography, Link } from '@mui/material';
+import { Box, styled, Typography, Link, Grid } from '@mui/material';
 import { GitHub, Instagram, Email } from '@mui/icons-material';
 
 const Banner = styled(Box)`
@@ -27,16 +27,31 @@ const Contact = () => {
         <Box>
             <Banner />
             <Wrapper>
-                <Typography variant="h3">Getting in touch is easy!</Typography>    
+                <Typography variant="h3" fontFamily={"Roboto"}>Alibino Problems are REAL !!!!</Typography>
                 <Text variant="h5">
-                    Reach out to me on
-                    <Link href="https://www.instagram.com/codeforinterview/" color="inherit" target="_blank">
-                        <Instagram/>
-                    </Link>
-                    or send me an Email 
-                    <Link href="mailto:codeforinterview@gmail.com?Subject=This is a subject" target="_blank" color="inherit">
-                        <Email />
-                    </Link>.
+                    Albinism problems remain unspoken. While discussing the problems with
+                    loved ones always helps, contacting for help to the right experts
+                    can help mitigate some pain. The following organizations work hard to make
+                    life for Albino's better everyday.
+                    <ul>
+                        <li>
+                            The National Organization for Albinism and Hypopigmentation (NOAH) <br></br>
+                            Website: <Link href="https://www.albinism.org/" color="inherit" target="_blank"> https://www.albinism.org/ </Link>
+                        </li>
+                        <li>
+                             Albinism – Kids Health For Kids  <br></br>
+                            Website: <Link href="https://www.kidshealth.org/" color="inherit" target="_blank"> https://www.kidshealth.org/ </Link>
+                        </li>
+                        <li>
+                             Vision for Tomorrow  <br></br>
+                            Website: <Link href="https://visionfortomorrow.org/albinism/" color="inherit" target="_blank"> https://visionfortomorrow.org/albinism/ </Link>
+                        </li>
+                        <li>
+                             Positive Exposure  <br></br>
+                            Website: <Link href="https://positiveexposure.org/" color="inherit" target="_blank"> https://positiveexposure.org/ </Link>
+                        </li>
+                    </ul>
+                            
                 </Text>
             </Wrapper>
         </Box>

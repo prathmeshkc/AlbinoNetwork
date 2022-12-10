aa3653b907c1685689f71a6a77883dfabb42e010
prathmeshneu <chaudhari.prat@northeastern.edu>
2022-12-10 00:41:07 -0500

    deplyed backend to heroku

--- Blog-Website/server/controller/image-controller.js
+++ Blog-Website/server/controller/image-controller.js
@@ -1,7 +1,8 @@
 import grid from 'gridfs-stream';
 import mongoose from 'mongoose';
 
-const url = 'http://localhost:8000';
+// const url = 'http://localhost:8000';
+const url = 'https://albinonetworkapi.herokuapp.com';
 
 
 let gfs, gridfsBucket;
